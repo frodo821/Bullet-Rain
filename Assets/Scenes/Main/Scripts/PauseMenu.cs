812d1db2f6cc525364974e0b35f837c8a5149d4c
Chihiro Sakai <sakaic2003@gmail.com>
2017-10-07 01:21:01 +0900

    Updated at 20171007 01:21 JST

---
+++ Assets/Scenes/Main/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject dialog;
+    public bool inPause = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            inPause = !inPause;
+            Time.timeScale = 1f;
+            dialog.gameObject.SetActive(false);
+        }
+        if (inPause)
+        {
+            Time.timeScale = 0f;
+            dialog.gameObject.SetActive(true);
+        }
+	}
+
+    public void CloseMenu()
+    {
+        Time.timeScale = 1f;
+        inPause = false;
+        dialog.gameObject.SetActive(false);
+    }
+}
